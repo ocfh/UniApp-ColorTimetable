d2806ae42aabdd2d36969aaa6544c4c006f321a6
ByteBlader <uliaxs0n@gmail.com>
2024-06-28 02:09:17 +0800

    Modify the weekly course

--- src/components/timetable/TimetableAction.vue
+++ src/components/timetable/TimetableAction.vue
@@ -1,12 +1,8 @@
 <script setup lang="ts">
 const props = defineProps<{ showCourseAction: boolean }>()
-
 const { parsedCourseList, originalWeekIndex, currentWeekIndex } = storeToRefs(useCourseStore())
-
 const { setCurrentWeekIndex } = useCourseStore()
-
 const scrollTo = ref('week0')
-
 watch(
   () => +props.showCourseAction + currentWeekIndex.value,
   () => {
@@ -14,10 +10,10 @@ watch(
       scrollTo.value = `week${currentWeekIndex.value - 1}`
   })
 </script>
-
 <template>
   <scroll-view
     class="transition-height duration-300 overflow-scroll whitespace-nowrap"
+	style="overflow: auto hidden; filter: drop-shadow(rgba(147, 197, 253, 0.5) 0px 0px 4px); background-color: rgba(0, 0, 0, 0.4); color: rgb(211, 211, 211);"
     :class="showCourseAction ? 'h-20' : 'h-0'" scroll-x scroll-with-animation :scroll-into-view="scrollTo"
   >
     <template v-for="(weeksTimetable, weeksIndex) of parsedCourseList" :key="weeksIndex">
@@ -25,22 +21,15 @@ watch(
         :id="`week${weeksIndex + 1}`" class="py-1 px-2 inline-block"
         @click="setCurrentWeekIndex(weeksIndex)"
       >
-        <div
+        <div style="margin-top:1rem;border-radius: 100px;"
           class="rounded-lg py-1 px-2"
           :class="originalWeekIndex === weeksIndex ? 'bg-gray-400/50 dark:!bg-op60' : currentWeekIndex === weeksIndex ? 'bg-gray-300 bg-op80 dark:!bg-op20' : ''"
+		  :style="originalWeekIndex === weeksIndex ? 'color: rgb(80, 191, 254)!important;box-shadow: rgb(81, 191, 254) 0px 2px 2px, rgb(81, 192, 255) 0px 0px 0px 1px inset!important;' : currentWeekIndex === weeksIndex ? 'color: rgb(80, 191, 254)!important;box-shadow: rgb(81, 191, 254) 0px 2px 2px, rgb(81, 192, 255) 0px 0px 0px 1px inset!important;' : 'box-shadow: rgba(255, 255, 255, 0.4) 0px 2px 2px, rgb(255, 255, 255) 0px 0px 0px 1px inset;'"
         >
           <div class="text-xs text-center mb-1">
-            {{ `第${weeksIndex + 1}周` }}
+            {{ `第 ${weeksIndex + 1} 周` }}
           </div>
-          <div class="h-10 w-10" grid="~ flow-col cols-5 rows-5">
-            <template v-for="(weekWeekTimetable, weekWeekIndex) of weeksTimetable" :key="weekWeekIndex">
-              <template v-if="weekWeekIndex < 5">
-                <template v-for="(item, _idx) of weekWeekTimetable" :key="_idx">
-                  <div class="rounded-full mx-auto h-1.5 w-1.5" :class="item ? 'bg-light-blue-500' : 'bg-gray-200'" />
-                </template>
-              </template>
-            </template>
-          </div>
+          
         </div>
       </div>
     </template>
--- src/components/timetable/TimetableHeader.vue
+++ src/components/timetable/TimetableHeader.vue
@@ -1,23 +1,19 @@
 <script setup lang="ts">
 import { weekTitle } from '~/stores/course'
-
 const {
   isStart, currentMonth, originalWeekIndex, currentWeekIndex,
   originalWeekWeekIndex, currentWeekDayArray,
 } = storeToRefs(useCourseStore())
-
 const isCurrentWeek = (weekIndex: number) => {
+
   if (!isStart.value)
     return false
-  if (!originalWeekIndex.value || !currentWeekIndex.value || !originalWeekWeekIndex.value)
-    return false
-
   return originalWeekIndex.value === currentWeekIndex.value && originalWeekWeekIndex.value === weekIndex
 }
 </script>
 
 <template>
-  <div class="h-10 shadow-sm px-1 top-0" grid="~ cols-[0.7fr_repeat(7,1fr)] gap-1">
+  <div class="h-10 shadow-sm px-1 top-0" grid="~ cols-[0.7fr_repeat(7,1fr)] gap-1" style="padding-right:0!important">
     <div class="flex font-medium text-sm items-center justify-center">
       {{ `${currentMonth}月` }}
     </div>
@@ -27,6 +23,7 @@ const isCurrentWeek = (weekIndex: number) => {
       flex="~ col" justify-evenly items-center
       b="y-transparent x-none t-4 b-4"
       :class="isCurrentWeek(index) ? 'bg-light-blue-300 !b-b-light-blue-500' : ''"
+	  :style="isCurrentWeek(index) ? 'border-bottom-color: #50bfff!important;box-shadow:0 3px 4px rgba(32,160,255,.5)' : ''"
     >
       <p class="font-medium">
         {{ weekTitle[index] }}
